928cb97f2e66ad14af73082c36b7c74323b6dbb2
ThasneemGit <itsmeneem@gmail.com>
2021-03-26 22:23:20 -0500

    first commit

---
+++ src/test/java/QaclickAcademy/QuickTest.java
@@ -0,0 +1,16 @@
+package QaclickAcademy;
+
+import org.testng.annotations.Test;
+
+@Test
+public class QuickTest {
+	public void Qtest()
+	{
+		System.out.println("Quick test");
+	}
+	public void change()
+	{
+		System.out.println("Chnage test case executed successfully");
+	}
+
+}
---
+++ src/test/java/QaclickAcademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package QaclickAcademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	
+	@Test
+	public void test()
+	{
+		System.out.println("Success");
+	}
+	@Test
+	public void test1()
+	{
+		System.out.println("Success yes");
+	}
+}
